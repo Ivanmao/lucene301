f491e7f4337a5b0754f9f512ad6ba3211a6d5d12
unknown <maoguohong@XS-JS-6D166.emoney.cn>
2014-11-25 10:59:02 +0800

    first commit for lucene301

---
+++ src/main/java/com/maogh/lucene301/App.java
@@ -0,0 +1,13 @@
+package com.maogh.lucene301;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
