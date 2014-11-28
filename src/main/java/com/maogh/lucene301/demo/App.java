ce8ae7c3329c21ba02c0413af03b6e6d918b4463
unknown <maoguohong@XS-JS-6D166.emoney.cn>
2014-11-28 14:29:52 +0800

    add dir demo

---
+++ src/main/java/com/maogh/lucene301/demo/App.java
@@ -0,0 +1,13 @@
+package com.maogh.lucene301.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
